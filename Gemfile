dd799eec9abffa907c9d1c77cbd154c6f0c4159e
Laurie Gloge <gloge@haathi-consulting.com>
2016-02-25 18:37:07 -0700

    added account controller

--- Gemfile
+++ Gemfile
@@ -23,7 +23,14 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# supports heroku
 gem 'rails_12factor', group: :production
+
+# authentication 
+gem 'devise', '~> 3.5', '>= 3.5.6'
+
+# makes messages prettier and easy to read
+gem 'awesome_print', '~> 1.6', '>= 1.6.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validate :email_is_unique, on: :create_account
+
+  after_create :create_account
+
+  def confirmation_required?
+  	false
+  end
+
+  private
+
+  def email_is_unique
+  	return false unless self.errors[:email].empty?
+  	# Do not valid email if errors are already present.
+  	Account.find_by_email(email).nil?
+  	errors.add(:email, " is already used")
+  end
+
+  def create_account
+  	account = Account.new(:email => email)
+  	account.save!
+  end
+end
