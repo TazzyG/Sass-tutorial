07d882854730c4f5726aef54343dc100633fc375
Laurie Gloge <gloge@haathi-consulting.com>
2016-03-02 16:19:34 -0700

    fixed typo in stripe webhooks init file

--- config/initializers/stripe_webhooks.rb
+++ config/initializers/stripe_webhooks.rb
@@ -8,43 +8,33 @@ StripeEvent.configure do |events|
   	#end 
 
   	# Charge failed/deleted for subscription
-  	events.subscribe 'customer.subscription.deleted' do |event|
+  	#Subscription failed/deleted
+	  events.subscribe 'customer.subscription.deleted' do |event|
 
-  		ap 'customer.subscription.deleted'
-  		ap event
-  		#Delete the customer's subscription from Account model
-  		subscription = event.data.object
-  		customer_id  = subscription.customer
+	    ap 'customer.subscription.deleted'
+	    ap event
 
-  		#move to account model
-  		account 								= Account.find_by_customer_id(customer_id)
-  		account.stripe_plan_id 	= ''
-  		#account.customer is unchanged
-  		account.active_until 		= Time.at(0).to_datetime
-  		account.save!
-  	end 
+	    #Delete the customer's subscription from Account model
+	    subscription = event.data.object
+	    customer_id  = subscription.customer
 
-  	# Charge succeeded for subscription
-  	events.subscribe 'customer.subscription.updated' do |event|
+	    #Move to account model
+	    account                = Account.find_by_customer_id(customer_id)
+	    account.stripe_plan_id = ''
+	    #account.customer is unchanged
+	    account.active_until   = Time.at(0).to_datetime
+	    account.save!
+	  end 
 
-  		ap 'customer.subscription.updated'
-  		ap event
-  		# Occurs whenever a subscription changes. 
-  		subscription 						= event.data.object
-  		account 								= Account.find_by_customer_id(customer_id)
-  		account.stripe_plan_id 	= subscription.plan.id
-  		#account.customer is unchanged
-  		account.active_until 		= Time.at(subscription.current_period_end).to_datetime
-  		account.save!
-  	end
+
 
     # Define subscriber behavior based on the event object
-    event.class       #=> Stripe::Event
-    event.type        #=> "charge.failed"
-    event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
-  end
+    #event.class       #=> Stripe::Event
+    #event.type        #=> "charge.failed"
+    #event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+  #end
 
-  events.all do |event|
+  #events.all do |event|
     # Handle all event types - logging, etc.
-  end
+  #end
 end
